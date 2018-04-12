d9feba395d34d0760a709c18451b627c12e5fec3
Frank Berger <fberger@b-factor.de>
2018-04-12 16:07:28 +0200

    guard7 dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-	'title'        => '(Sudhaus7) Data Vault',
+	'title'        => '(Sudhaus7) Guard',
 	'description'  => 'guard7',
 	'category'     => 'fe',
 	'state'        => 'beta',
