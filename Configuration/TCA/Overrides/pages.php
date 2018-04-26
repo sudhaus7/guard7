6a1b8e5d6161dca5d5df3c79212c80631a427cec
Frank Berger <fberger@b-factor.de>
2018-04-26 16:33:10 +0200

    renamed typoscript file to t3s extension

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -5,7 +5,7 @@ if (!defined('TYPO3_MODE')) die();
 call_user_func(function () {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
 	    'guard7',
-        'Configuration/PageTSconfig/page.ts',
+        'Configuration/PageTSconfig/page.t3s',
 	    'Seitendefinitionen Guard7'
     );
 });
