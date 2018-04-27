0c35ef5ae532500d71ba63e05a9ca81ef7a2b524
Frank Berger <fberger@b-factor.de>
2018-04-27 14:47:02 +0200

    updates for backend module

--- Resources/Public/JavaScript/Backend.js
+++ Resources/Public/JavaScript/Backend.js
@@ -3,9 +3,14 @@ define(['jquery', 'TYPO3/CMS/Guard7/Guard7Tools'], function ($, Guard7Tools) {
     $('#createkey button').click(function(e) {
         e.stopPropagation();
         e.preventDefault();
-        console.log($('#loading'));
+        var depth = $(this).val();
+        var password =  $('#createkey input[name="password"]').val();
+        if (!password || password.length < 1) {
+            password = null;
+        }
+        //console.log($('#loading'));
         $('#loading').removeClass('off');
-        Guard7Tools.createPrivateKey(null, 2048)
+        Guard7Tools.createPrivateKey(password, depth)
         .then(function(keypair) {
             $('#createkey textarea[name="privatekey"]').val(keypair.privateKey);
             $('#createkey textarea[name="publickey"]').val(keypair.publicKey);
