8b85401fe44892798040f166059a782618b6900a
Frank Berger <fberger@b-factor.de>
2019-12-16 12:59:23 +0100

    codeception boilerplate

---
+++ Tests/acceptance/BackendCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class BackendCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
