1ea49b9698582768f1c3e67781791c6785480935
Frank Berger <fberger@b-factor.de>
2020-01-15 17:30:33 +0100

    new description

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,11 +2,12 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => '(Sudhaus7) Guard7',
-    'description' => 'guard7',
-    'category' => 'fe',
-    'state' => 'beta',
+    'description' => 'A TYPO3 extension for encrypting and decrypting data in frontend and backend, including public and private key management.',
+    'category' => 'misc',
+    'state' => 'stable',
     'author' => 'Frank Berger',
     'author_email' => 'fberger@sudhaus7.de',
+    'author_company' => 'Sudhaus7 https://sudhaus7.de/',
     'version' => '8.0.0',
     'constraints' => [
         'depends' => [
