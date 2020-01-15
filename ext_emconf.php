9fa59b1649cd53969c1c1b59d1b8bb2c1b7beaf8
Frank Berger <fberger@b-factor.de>
2020-01-15 17:20:03 +0100

    ext_emconf.php constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,4 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Frank Berger',
     'author_email' => 'fberger@sudhaus7.de',
     'version' => '8.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-8.7.999',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
 ];
